60598ff8cc8f2fa1b5f40911e5b3c24c1a4becc5
Javier Gibert <adelgibert@gmail.com>
2023-05-26 14:58:10 +1200

    removed container

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import { Container } from "reactstrap";
 import MessengerApp from "./components/MessengerApp";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <Container> */}
     <MessengerApp />
-    {/* </Container> */}
   </React.StrictMode>
 );
