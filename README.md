2d2fc940003c4f79c7f58b818c5ea351c65a423a
Javier Gibert <adelgibert@gmail.com>
2023-05-29 12:08:30 +1200

    added error boundaries to app

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ I've been having trouble with npm packages. I've switched to yarn but left the p
 
 - Fetch Real Data
 - Error Handling
+  - Boundaries ✓
+  - Event Listeners
 - Loading States
 - Unit Testing
 - Redux Implementation
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import MessengerApp from "./components/MessengerApp";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <MessengerApp />
+    <ErrorBoundary>
+      <MessengerApp />
+    </ErrorBoundary>
   </React.StrictMode>
 );
